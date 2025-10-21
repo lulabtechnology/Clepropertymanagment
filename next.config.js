dd04f91afcf9008f831dee4f8d211c052ce1ab15
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-10-21 13:15:53 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      // Habilita si usas imágenes remotas en el futuro
-      { protocol: "https", hostname: "**" }
-    ]
+    formats: ["image/avif", "image/webp"],   // <-- formatos modernos más nítidos
+    // deviceSizes por defecto ya incluye 1920/2048/3840; si quieres, puedes forzar:
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 2560, 3840]
   }
 };
 module.exports = nextConfig;
